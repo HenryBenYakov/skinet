d9249c91a4d9e92a4f1ff85b1337fc348c6e6ccf
Henry Ben Yakov <henrybenyakov1@gmail.com>
2023-06-26 21:04:39 +0300

    Adding AutoMapper

---
+++ Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
@@ -0,0 +1,20 @@
+using Core.Entities;
+
+namespace Core.Specifications
+{
+    public class ProductsWithTypesAndBrandsSpecification : BaseSpecification<Product>
+    {
+
+        public ProductsWithTypesAndBrandsSpecification()
+        {
+            AddInlude(x => x.ProductType);
+            AddInlude(x => x.ProductBrand);
+        }
+
+        public ProductsWithTypesAndBrandsSpecification(int id) : base(x => x.Id == id)
+        {
+            AddInlude(x => x.ProductType);
+            AddInlude(x => x.ProductBrand);
+        }
+    }
+}
